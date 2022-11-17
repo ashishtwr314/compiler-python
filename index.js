fddd0e98d7e970f83d5cb261b3c472e9f1d8f0c1
Ashish Tiwari <ashishtwr314@gmail.com>
2022-11-18 01:21:52 +0530

    initla push

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { PythonShell } = require("python-shell");
+
+const PORT = 8080;
+const app = express();
+app.use(cors());
+app.options("*", cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.post("/compile", (req, res) => {
+  try {
+    let _code = req.body.code;
+    const code = _code.trim();
+
+    PythonShell.runString(code, null, function (err, results) {
+      if (err) {
+        return res.status(200).send({ err: true, output: err.message });
+      }
+
+      res.status(200).send({ err: false, output: results });
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
